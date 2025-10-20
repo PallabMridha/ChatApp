4145896f590cc61163871cf8fc18771acb885ee9
eCommerce-Website~ <pallabmridha@gmail.com>
2025-10-20 13:55:19 +0600

    bite

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import flowbiteReact from "flowbite-react/plugin/vite";
+
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
+  plugins: [react(), tailwindcss(),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
